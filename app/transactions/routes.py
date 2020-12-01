f89789b072b29692ad855b2c930e72fb30d84fba
shramkoartem <art.shramko@gmail.com>
2020-12-01 22:37:26 +0100

    function push_items_db reduce amount of items sold, commit changes to Items db

--- app/transactions/routes.py
+++ app/transactions/routes.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from flask import request
 from flask import jsonify
+from flask import session
 from app.transactions import bp
+from app.models import Item, Transaction
+from app import db
+
 
 
 
@@ -19,7 +23,33 @@ def transaction():
 
 @bp.route('/push_basket', methods=["POST"])
 def push_basket():
-    rData = request.get_json()
-    print(rData)
+    """
+    1. Receives transaction JSON from PUSH request
+    2. Substracts amounts of respective items from Items DB Table
+        (matched by barcodes)
+    3. Pushes transaction to Transactions DB Table
+    """
+
+    # Receives transaction JSON from PUSH request
+    request_data = request.get_json()
+    print(request_data)
+
+    # Update item amounts in the Items db
+    if request_data:
+        basket = request_data
+        for item in basket:
+            push_items_db(item)
+         
+
     return jsonify({'status' : 'success'})
     
+
+def push_items_db(data):
+    """
+    Updates amount of item in Items DB
+    :data: (dict) item = {barcode, name, amount, price}
+    """
+    item = Item.query.filter_by(barcode = data["barcode"]).first()
+    if item:
+        item.amount -= data["amount"]
+        db.session.commit() 
