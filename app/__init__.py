d855d3f69322d91bc1954a2d6a59f00e8753ac7d
shramkoartem <art.shramko@gmail.com>
2020-12-01 22:59:59 +0100

    added error logger

--- app/__init__.py
+++ app/__init__.py
@@ -45,6 +45,23 @@ def create_app(config_class=Config):
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix="/auth")
 
+    # Error logging
+    if not app.debug and not app.testing:    
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/imij.log',
+                                            maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('IMIJ app')
+
+
     return app
 
 
