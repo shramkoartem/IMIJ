5d91f7191fb4d248dccad2fa9d52e59fac805450
katharina-spiecker <kathispiecker@gmail.com>
2020-12-03 14:35:47 +0100

    basket module transformed to react class component

---
+++ ui/TableHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function THeader(props){
+    if(props.basket.length>0){
+        return (
+            <thead class="thead-light" id="selected-items-thead">
+                <tr>
+                    <th>Item</th>
+                    <th>Price</th>
+                    <th>Amount</th>
+                    <th></th>
+                </tr>
+            </thead>
+        )
+    } else {
+        return null;
+    }
+}
